abd7c3d216e0c10bded83bc43a19c96804e0b62a
TheOpti <wieckowski.piotr.93@gmail.com>
2022-05-05 20:45:54 +0200

    Add Playground plugin

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
 import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled } from 'apollo-server-core';
 
-// 1
+// 1 GraphQL schema
 const typeDefs = `
   type Query {
     info: String!
   }
 `
 
-// 2
+// 2 actual implementation of the GraphQL schema
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`
   }
 }
 
-// 3
+// 3 Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 })
 
 server
