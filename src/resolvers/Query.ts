332f8141694d657e600d945821c069ebee691844
TheOpti <wieckowski.piotr.93@gmail.com>
2024-06-26 22:34:33 +0200

    Improve types for Query - feed

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,4 +1,8 @@
-export const feed = async (parent: any, args: any, context: any) => {
+import { Prisma } from '@prisma/client';
+import { LinkOrderByInput, QueryFeedArgs } from '../generated/graphql';
+import { GraphQLContext } from '../types';
+
+export const feed = async (_: {}, args: QueryFeedArgs, context: GraphQLContext) => {
   const where = args.filter
     ? {
         OR: [{ description: { contains: args.filter } }, { url: { contains: args.filter } }]
@@ -7,8 +11,32 @@ export const feed = async (parent: any, args: any, context: any) => {
 
   return await context.prisma.link.findMany({
     where,
-    skip: args.skip,
-    take: args.take,
-    orderBy: args.orderBy
+    skip: args.skip ?? undefined,
+    take: args.take ?? undefined,
+    orderBy: convertOrderBy(args.orderBy)
   });
+};
+
+export const convertOrderBy = (
+  orderBy: LinkOrderByInput | null | undefined
+): Prisma.LinkOrderByWithRelationInput | undefined => {
+  if (!orderBy) {
+    return undefined;
+  }
+
+  const result: Prisma.LinkOrderByWithRelationInput = {};
+
+  if (orderBy.createdAt) {
+    result.createdAt = orderBy.createdAt.toLowerCase() as Prisma.SortOrder;
+  }
+
+  if (orderBy.description) {
+    result.description = orderBy.description.toLowerCase() as Prisma.SortOrder;
+  }
+
+  if (orderBy.url) {
+    result.url = orderBy.url.toLowerCase() as Prisma.SortOrder;
+  }
+
+  return result;
 };
