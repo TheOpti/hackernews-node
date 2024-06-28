7f3f2a0c0571ed363ac2e7b0345027560c9cd568
TheOpti <wieckowski.piotr.93@gmail.com>
2024-06-28 21:23:01 +0200

    Improve logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,14 @@
+import path from 'path';
 import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const customFormatter = format.printf(({ level, message, timestamp, ...metadata }) => {
+  let msg = `${timestamp} [${level}]: ${message} `;
+  if (Object.keys(metadata).length > 0) {
+    msg += JSON.stringify(metadata);
+  }
+  return msg;
+});
 
 const logger = createLogger({
   level: 'info',
@@ -8,21 +18,33 @@ const logger = createLogger({
     }),
     format.errors({ stack: true }),
     format.splat(),
-    format.json()
+    customFormatter
   ),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' })
-  ]
+  transports: [new transports.File({ filename: path.join('logs', 'error.log'), level: 'error' })]
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple())
+      format: format.combine(
+        format.colorize(),
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        customFormatter
+      )
     })
   );
 }
+
+const fileRotateTransport = new DailyRotateFile({
+  filename: path.join('logs', 'application-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '14d',
+  auditFile: './logs/audit-file.json'
+});
+
+logger.add(fileRotateTransport);
 
 export { logger };
