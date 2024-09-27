bf38e151db075b72a952dde7864f24136203e63e
TheOpti <wieckowski.piotr.93@gmail.com>
2024-09-27 21:54:53 +0200

    Fix for getting user comments and votes

--- src/resolvers/entities/User.ts
+++ src/resolvers/entities/User.ts
@@ -2,5 +2,11 @@ import { User } from '../../generated/graphql';
 import { GraphQLContext } from '../../types';
 
 export const links = (parent: User, _: {}, context: GraphQLContext) => {
-  return context.prisma.link.findMany({ where: { postedById: parent.id } });
+  return context.prisma.link.findMany({
+    where: { postedById: parent.id },
+    include: {
+      comments: true,
+      votes: true
+    }
+  });
 };
